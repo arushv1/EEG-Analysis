ba83f3c7643d54e295c33f7b1a613d3fefbccdda
arushv1 <arushv16@gmail.com>
2025-08-15 15:01:31 +0200

    Adding more files

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -2,6 +2,9 @@ import pandas as pd
 import torch
 from sklearn.preprocessing import StandardScaler
 from src.config.config import Config
+from src.exception import CustomException
+from src.logger import logging
+import sys
 
 class DataTransformation:
     def __init__(self):
@@ -11,16 +14,27 @@ class DataTransformation:
         """
         Fit the scaler to the data and transform it.
         """
-        train_df = pd.read_csv(Config.PROCESSED_TRAIN_PATH)
-        X = self.scaler.fit_transform(train_df.iloc[:, 1:-1])
-        y = train_df.iloc[:, -1]
-        return torch.tensor(X, dtype=torch.float32), torch.tensor(y.values, dtype=torch.long)
+        try:
+            train_df = pd.read_csv(Config.PROCESSED_TRAIN_PATH)
+            X = self.scaler.fit_transform(train_df.iloc[:, 1:-1])
+            y = train_df.iloc[:, -1]
+            logging.info("Training Data transformation completed successfully.")
+            return torch.tensor(X, dtype=torch.float32), torch.tensor(y.values, dtype=torch.long)
+    
+        except Exception as e:
+            raise CustomException(e, sys)
+            
     
     def transform_test(self):
         """
         Transform the test data using the fitted scaler.
         """
-        test_df = pd.read_csv(Config.PROCESSED_TEST_PATH)
-        X = self.scaler.transform(test_df.iloc[:, 1:-1])
-        y = test_df.iloc[:, -1]
-        return torch.tensor(X, dtype=torch.float32), torch.tensor(y.values, dtype=torch.long)
+        try:
+            test_df = pd.read_csv(Config.PROCESSED_TEST_PATH)
+            X = self.scaler.transform(test_df.iloc[:, 1:-1])
+            y = test_df.iloc[:, -1]
+            logging.info("Test Data transformation completed successfully.")
+            return torch.tensor(X, dtype=torch.float32), torch.tensor(y.values, dtype=torch.long)
+    
+        except Exception as e:
+            raise CustomException(e, sys)
